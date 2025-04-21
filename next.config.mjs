a28e0b9e31ad979f40d3ba4b1156809f7dec8890
virgile popote <virgilepopote@gmail.com>
2025-04-21 18:14:41 -0400

    Suppression de l'option i18n incompatible avec export

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
-import i18nConfig from './next-i18next.config.js';
-const { i18n } = i18nConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n,
   output: 'export',
   distDir: '.next',
   images: {
