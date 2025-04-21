453a1e17c27c83301fe1ee5ba829520e6c625ca5
virgile popote <virgilepopote@gmail.com>
2025-04-21 18:14:06 -0400

    Simplification de la configuration pour Netlify

--- next.config.mjs
+++ next.config.mjs
@@ -4,25 +4,12 @@ const { i18n } = i18nConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
-  output: 'standalone',
+  output: 'export',
+  distDir: '.next',
   images: {
-    domains: [],
     unoptimized: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png|ico|webp)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          }
-        ],
-      },
-    ]
-  },
+  trailingSlash: true
 };
 
 export default nextConfig;
