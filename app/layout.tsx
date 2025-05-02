aa16f33717d58e24b8db7eabd344f271581679a2
virgile popote <virgilepopote@gmail.com>
2025-05-02 06:00:44 -0400

    baliseAnalytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import I18nProvider from "./components/i18nProvider";
 import Favicon from "./components/Favicon";
 import MouseHalo from "./components/MouseHalo";
 import Footer from "./components/Footer";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,29 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   title: "Création site internet Guadeloupe – Virgile Popote, développeur web freelance",
   description: "Développeur web freelance en Guadeloupe. Création de sites vitrines, e-commerce et sur-mesure pour entreprises et entrepreneurs. Contactez-moi pour booster votre présence en ligne.",
+  keywords: "développeur web, freelance, Guadeloupe, création site internet, site vitrine, e-commerce, next.js, développeur frontend",
+  authors: [{ name: "Virgile Popote", url: "https://virgilepopote.com" }],
+  creator: "Virgile Popote",
+  publisher: "Virgile Popote",
+  metadataBase: new URL("https://virgilepopote.com"),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'fr': '/',
+      'en-US': '/en',
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
   icons: [
     { rel: "icon", url: "/favicon.ico", sizes: "any" },
     { rel: "icon", url: "/favicon.png", type: "image/png", sizes: "200x200" },
@@ -54,6 +78,17 @@ export default function RootLayout({
     <html lang="fr" suppressHydrationWarning>
       <head>
         <Favicon />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5VTGKQFS');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
         {/* Balises Open Graph */}
         <meta property="og:title" content="Création site internet Guadeloupe – Virgile Popote, développeur web freelance" />
         <meta property="og:description" content="Développeur web freelance en Guadeloupe. Création de sites vitrines, e-commerce et sur-mesure pour entreprises et entrepreneurs." />
@@ -100,6 +135,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${jetbrainsMono.variable} antialiased bg-white dark:bg-black`}
         suppressHydrationWarning
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{ __html: `
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5VTGKQFS"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        `}} />
+        {/* End Google Tag Manager (noscript) */}
         <MouseHalo />
         <div className="flex flex-col min-h-screen">
           <ThemeProvider
